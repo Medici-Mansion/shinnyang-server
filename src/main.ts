ae5c1803b4ca0d3680220dcf5bfc801c16cf8c39
pilsung choi <81282655+pilsung-choi@users.noreply.github.com>
2023-12-12 23:01:17 +0900

    Feat : global middleware 생성 (#17)
    * initial commit
    
    * Feat : global middleware 생성
    
    Feat : app module에 미들웨어 적용
    
    Swagger 적용
    
    typeorm 설치 및 적용
    
    인터셉터 생성
    
    * Feat : global middleware 생성
    
    Feat : app module에 미들웨어 적용
    
    Swagger 적용
    
    typeorm 설치 및 적용
    
    인터셉터 생성
    
    ---------
    
    Co-authored-by: Sungho95 <80394894+Sungho95@users.noreply.github.com>
    Co-authored-by: raymondanything <akdfhr2@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Shinnyang Project')
+    .setDescription('The Shinnyang Project description')
+    .setVersion('1.0')
+    .addTag('cat')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
+
 }
 bootstrap();
