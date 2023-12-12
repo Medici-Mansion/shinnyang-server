5f158771369b764a5e3b37c63edf9103578c9cbe
Raymond <akdfhr2@gmail.com>
2023-12-12 23:01:47 +0900

    0.0.5 (#18)
    * [CHORE] HOTFIX 동기화 (#12)
    
    * chore: add Dockerfile (#4)
    
    * test: 배포 트리거용 수정사항 적용
    
    * 0.0.2 (#7)
    
    * chore: add cloudbuild
    
    * 0.0.2 (#7)
    
    * 0.0.2 (#9)
    
    * 0.0.2 (#10)
    
    * 0.0.2 (#11)
    
    * Update cloudbuild.yaml
    
    * test:  cicd 적용 확인을 위한 healthcheck 엔드포인트 추가
    
    * Feat : global middleware 생성 (#17)
    
    * initial commit
    
    * Feat : global middleware 생성
    
    Feat : app module에 미들웨어 적용
    
    Swagger 적용
    
    typeorm 설치 및 적용
    
    인터셉터 생성
    
    * Feat : global middleware 생성
    
    Feat : app module에 미들웨어 적용
    
    Swagger 적용
    
    typeorm 설치 및 적용
    
    인터셉터 생성
    
    ---------
    
    Co-authored-by: Sungho95 <80394894+Sungho95@users.noreply.github.com>
    Co-authored-by: raymondanything <akdfhr2@gmail.com>
    
    ---------
    
    Co-authored-by: pilsung choi <81282655+pilsung-choi@users.noreply.github.com>
    Co-authored-by: Sungho95 <80394894+Sungho95@users.noreply.github.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,35 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
-  imports: [],
+  imports: [
+    // 아직 어떤 db를 쓸지 정하지 않았음
+    // TypeOrmModule.forRoot({
+    //   type: '',
+    //   host: '',
+    //   port: ,
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE,
+    //   autoLoadEntities: ,
+    //   keepConnectionAlive: ,
+    //   migrations: ,
+    //   charset: '',
+    //   synchronize: false,
+    //   logging: true,
+    // }),
+    // TypeOrmModule.forFeature([
+    //   // 사용할 모듈들 입력
+    // ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
