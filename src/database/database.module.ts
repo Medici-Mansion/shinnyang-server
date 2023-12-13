5ee3f2c4ed7ad6a6ed0c8fc088e1b4d04f0b8cda
pilsung-choi <vlftjd159753@gmail.com>
2023-12-14 01:16:36 +0900

    폴더 생성

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,6 +4,20 @@ import { User } from 'src/entities/Users';
 import { UserModule } from 'src/users/user.module';
 
 @Module({
+  // imports: [
+  //   TypeOrmModule.forRoot({
+  //     type: 'postgres',
+  //     host: 'localhost', //process.env.DB_HOST,
+  //     port: 5432, //+process.env.DB_PORT,
+  //     username: 'pilsungchoi', // process.env.DB_USER,
+  //     password: 'cps159753', //process.env.DB_PWD,
+  //     database: 'postgres', // process.env.DB_NAME,
+  //     synchronize: true,
+  //     logging: true,
+  //     entities: [User],
+  //   }),
+  //   UserModule,
+  // ],
   imports: [
     TypeOrmModule.forRootAsync({
       useFactory() {
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  //   @Post()
+  //   create(@Body() createUserDto: CreateUserDto) {
+  //     // 작업들
+
+  //       return userdata: CreateUserDto
+  //   }
+}
+
+// 이메일 ,닉네임
+
+// 닉네임 updateUserDto
