b96236171f3022e9bb907050c2269a7982eaf5a8
pilsung-choi <vlftjd159753@gmail.com>
2023-12-14 00:15:25 +0900

    postgresql set env

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,18 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { UserService } from './user.service';
+import { Controller } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {
-  constructor(private readonly userService: UserService) {}
-
-  //   @Post()
-  //   create(@Body() createUserDto: CreateUserDto) {
-  //     // 작업들
-
-  //       return userdata: CreateUserDto
-  //   }
-}
-
-// 이메일 ,닉네임
-
-// 닉네임 updateUserDto
+export class UserController {}
