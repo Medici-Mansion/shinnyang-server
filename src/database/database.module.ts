ae69f1468ac426603f571c43d55b770de55f62af
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-12-14 01:44:54 +0900

    [박성호] SNP-38 env 환경설정 변경

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/entities/user';
 
 @Module({
   imports: [
@@ -21,45 +20,3 @@ import { User } from 'src/entities/user';
   ],
 })
 export class DatabaseModule {}
-
-// @Module({
-//   imports: [
-//     TypeOrmModule.forRootAsync({
-//       useFactory() {
-//         return {
-//           type: 'postgres',
-//           host: 'db.vuqseyvrmvlezhuuxanm.supabase.co',
-//           port: 5432,
-//           username: 'postgres',
-//           password: 'fourfour-go123!@',
-//           database: 'shinnyang',
-//           entities: [User],
-//           synchronize: true,
-//           logging: true,
-//         };
-//       },
-//     }),
-//   ],
-// })
-// export class DatabaseModule {}
-
-// @Module({
-//   imports: [
-//     TypeOrmModule.forRootAsync({
-//       useFactory() {
-//         return {
-//           type: 'postgres',
-//           host: process.env.DB_HOST,
-//           port: +process.env.DB_PORT,
-//           username: process.env.DB_USER,
-//           password: process.env.DB_PWD,
-//           database: process.env.DB_NAME,
-//           entities: [User],
-//           synchronize: true,
-//           logging: true,
-//         };
-//       },
-//     }),
-//   ],
-// })
-// export class DatabaseModule {}
