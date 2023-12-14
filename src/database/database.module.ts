6e489c660f7d7d9a122a30c3db2ba13332c491e1
pilsung-choi <vlftjd159753@gmail.com>
2023-12-15 01:36:54 +0900

    common 엔터티, 인터페이스 통일화

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,23 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/entities/Users';
-import { UserModule } from 'src/users/user.module';
 
 @Module({
-  // imports: [
-  //   TypeOrmModule.forRoot({
-  //     type: 'postgres',
-  //     host: 'localhost', //process.env.DB_HOST,
-  //     port: 5432, //+process.env.DB_PORT,
-  //     username: 'pilsungchoi', // process.env.DB_USER,
-  //     password: 'cps159753', //process.env.DB_PWD,
-  //     database: 'postgres', // process.env.DB_NAME,
-  //     synchronize: true,
-  //     logging: true,
-  //     entities: [User],
-  //   }),
-  //   UserModule,
-  // ],
   imports: [
     TypeOrmModule.forRootAsync({
       useFactory() {
@@ -34,7 +18,6 @@ import { UserModule } from 'src/users/user.module';
         };
       },
     }),
-    UserModule,
   ],
 })
 export class DatabaseModule {}
