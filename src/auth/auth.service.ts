3e47ac98fc52a7edbcbaab1e52a202d51e17196b
pilsung-choi <vlftjd159753@gmail.com>
2023-12-16 19:50:20 +0900

    Feat : "auth 엑세스 토큰 받기까지 구현 (미완성)"

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,66 @@
 import { Injectable } from '@nestjs/common';
 import { AuthRepository } from './auth.repository';
+import {
+  PostAccessTokenRequestDto,
+  PostAccessTokenResponseDto,
+} from './dtos/create-auth.dtos';
+
+import axios from 'axios';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly authRepo: AuthRepository) {}
+  constructor(
+    private readonly authRepo: AuthRepository,
+    private readonly http: HttpService,
+  ) {}
 
-  async createToken() {
-    return await this.authRepo.createtoken();
-    // 비즈니스 로직
+  // async gettoken(
+  //   postAccessTokenRequestDto: PostAccessTokenRequestDto,
+  // ): Promise<PostAccessTokenResponseDto> {
+  //   const accessToken = axios.post(
+  //     `https://kauth.kakao.com/oauth/token`,
+  //     {
+  //       grant_type: 'authorization_code',
+  //       client_id: '835ecc8c24480280c0a8164b9e366263',
+  //       code: postAccessTokenRequestDto.code,
+  //       redirect_uri: 'https://www.catsnewyear.site/',
+  //     },
+  //     {
+  //       headers: {
+  //         'Content-Type': 'application/x-www-form-urlencoded',
+  //         'Access-Control-Allow-Origin': '*',
+  //       },
+  //     },
+  //   );
+  //   // redis 넣어주고
+  //   //
+  //   console.log(accessToken);
+  //   return;
+  // }
+
+  async kakaoLogin(
+    postAccessTokenRequestDto: PostAccessTokenRequestDto,
+  ): Promise<PostAccessTokenResponseDto> {
+    const { code } = postAccessTokenRequestDto;
+    const { data } = await axios.post(
+      `https://kauth.kakao.com/oauth/token`,
+      {
+        grant_type: 'authorization_code',
+        client_id: process.env.KAKAO_APIKEY,
+        code,
+        redirect_uri: process.env.REDIRECT_URL,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Access-Control-Allow-Origin': '*',
+        },
+      },
+    );
+    // redis 넣어주고
+    //
+    console.log('data : ', data);
+    return data;
   }
 }
