8f5ee29e9853153b7aad8d2e252fdcd0d20c5513
pilsung-choi <vlftjd159753@gmail.com>
2023-12-16 19:50:20 +0900

    Feat : "auth 엑세스 토큰 받기까지 구현 (미완성)"

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,37 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Header,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
+import {
+  PostAccessTokenRequestDto,
+  PostAccessTokenResponseDto,
+} from './dtos/create-auth.dtos';
 
 @Controller('auth')
 @ApiTags('kakao auth API')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  @ApiOperation({})
-  @ApiOkResponse({})
-  async createAccessToken() {
-    return this.authService.createToken();
+  @Get('/login')
+  @Header('Content-Type', 'text/html')
+  async kakaoRedirect(@Res() res: Response): Promise<void> {
+    const url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.KAKAO_APIKEY}&redirect_uri=${process.env.REDIRECT_URL}`;
+    res.redirect(url);
+  }
+
+  @Get('/kakao')
+  async getKakaoInfo(
+    @Query() postAccessTokenRequestDto: PostAccessTokenRequestDto,
+  ): Promise<PostAccessTokenResponseDto> {
+    return this.authService.kakaoLogin(postAccessTokenRequestDto);
   }
 }
 
