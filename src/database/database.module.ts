45644b57969f13d72f981680c57f3151a36e7e8c
pilsung-choi <vlftjd159753@gmail.com>
2023-12-17 00:59:15 +0900

    db, redis 설정

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,19 +2,22 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/Users';
 import { UserModule } from 'src/users/user.module';
-
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost', //process.env.DB_HOST,
-      port: 5432, //+process.env.DB_PORT,
-      username: 'pilsungchoi', // process.env.DB_USER,
-      password: 'cps159753', //process.env.DB_PWD,
-      database: 'postgres', // process.env.DB_NAME,
-      synchronize: process.env.NODE_ENV !== 'production',
-      logging: process.env.NODE_ENV !== 'production',
-      entities: [__dirname + './../**/**.entity{.ts,.js}'],
+    TypeOrmModule.forRootAsync({
+      useFactory() {
+        return {
+          type: 'postgres',
+          host: process.env.DB_HOST, //process.env.DB_HOST,
+          port: +process.env.DB_PORT, //+process.env.DB_PORT,
+          username: process.env.DB_USER, // process.env.DB_USER,
+          password: process.env.DB_PWD, //process.env.DB_PWD,
+          database: process.env.DB_NAME, //process.env.DB_NAME, // process.env.DB_NAME,
+          synchronize: process.env.NODE_ENV !== 'production',
+          logging: process.env.NODE_ENV !== 'production',
+          entities: [__dirname + './../**/**.entity{.ts,.js}'],
+        };
+      },
     }),
   ],
 })
