8de1a16f9dc543f47a738baefa449d08f445c049
pilsung-choi <vlftjd159753@gmail.com>
2023-12-17 00:59:15 +0900

    db, redis 설정

--- global.d.ts
+++ global.d.ts
@@ -10,5 +10,16 @@ declare namespace NodeJS {
     readonly REDIS_PASSWORD: string;
     readonly REDIRECT_URL: string;
     readonly KAKAO_APIKEY: string;
+    readonly REDIS_HOST: string;
+    readonly REDIS_PORT: string;
+    readonly REDIS_PASSWORD: string;
+    readonly REDIS_HOST_LOCAL: string;
+    readonly REDIS_PORT_LOCAL: string;
+    readonly REDIS_PASSWORD_LOCAL: string;
+    readonly DB_HOST_LOCAL: string;
+    readonly DB_PORT_LOCAL: string;
+    readonly DB_USER_LOCAL: string;
+    readonly DB_PWD_LOCAL: string;
+    readonly DB_NAME_LOCAL: string;
   }
 }
