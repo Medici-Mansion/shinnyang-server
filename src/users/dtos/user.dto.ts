94c3d4780baaa7352f4f29227e359f14b7966b83
pilsung-choi <vlftjd159753@gmail.com>
2023-12-16 20:09:24 +0900

    Fix : 설명 추가

--- src/users/dtos/user.dto.ts
+++ src/users/dtos/user.dto.ts
@@ -1,11 +1,22 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsNumber } from 'class-validator';
 
-export class CreateUserDto {
+export class UserDto {
+  @ApiProperty({ description: '회원 아이디' })
+  @IsNumber()
+  id: number;
+
+  @ApiProperty({ description: '회원 이메일' })
   @IsEmail()
   email: string;
 
-  @IsNotEmpty()
-  password: string;
+  @ApiProperty({ description: '회원 닉네임' })
+  nickname: string;
+
+  @ApiProperty({
+    description: '회원의 현재 상태 e.g. (active, sleep, withdrawal',
+  })
+  status: string;
 }
 
 export class ROUserDto {}
