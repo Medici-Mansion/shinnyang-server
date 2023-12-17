59c7b528f2404002d760586a341154936f9f4065
pilsung-choi <vlftjd159753@gmail.com>
2023-12-17 16:08:47 +0900

    OAuth2 변경 및 jwt 초기 설정

--- global.d.ts
+++ global.d.ts
@@ -7,19 +7,11 @@ declare namespace NodeJS {
     readonly DB_PWD: string;
     readonly DB_NAME: string;
     readonly REDIS_HOST: string;
-    readonly REDIS_PASSWORD: string;
-    readonly REDIRECT_URL: string;
-    readonly KAKAO_APIKEY: string;
-    readonly REDIS_HOST: string;
     readonly REDIS_PORT: string;
     readonly REDIS_PASSWORD: string;
-    readonly REDIS_HOST_LOCAL: string;
-    readonly REDIS_PORT_LOCAL: string;
-    readonly REDIS_PASSWORD_LOCAL: string;
-    readonly DB_HOST_LOCAL: string;
-    readonly DB_PORT_LOCAL: string;
-    readonly DB_USER_LOCAL: string;
-    readonly DB_PWD_LOCAL: string;
-    readonly DB_NAME_LOCAL: string;
+    readonly GOOGLE_API_KEY: string;
+    readonly GOOGLE_AUTH_CLIENT_ID: string;
+    readonly GOOGLE_REDIRECT_URL: string;
+    readonly GOOGLE_CLIENT_SECRET: string;
   }
 }
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import axios from 'axios';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -9,8 +9,43 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-  @Get("health")
+
+  @Get('health')
   healthCheck() {
-    return true
+    return true;
+  }
+
+  @Get('login') // access-tokem을 가져오고 회원 insert?
+  async login(@Query('code') code: string, @Query('scope') scope: string) {
+    // DTO로 바꿔야함
+    const form = new FormData();
+    form.append('client_id', process.env.GOOGLE_AUTH_CLIENT_ID);
+    form.append('client_secret', process.env.GOOGLE_CLIENT_SECRET);
+    form.append('code', code);
+    form.append('grant_type', 'authorization_code');
+    form.append('redirect_uri', process.env.GOOGLE_REDIRECT_URL);
+    try {
+      const response = await axios.post(
+        'https://oauth2.googleapis.com/token',
+        form,
+      );
+      // console.log('access_token =', response.data);
+      if (response.data['access_token']) {
+        const userUrl = 'https://www.googleapis.com/oauth2/v2/userinfo';
+
+        const user = await axios.get(userUrl, {
+          params: {
+            access_token: response.data['access_token'],
+          },
+        });
+        // console.log('user-datas = ',user.data);
+        return user.data;
+      }
+      console.log(response.data);
+      return response.data;
+    } catch (err) {
+      console.log(err);
+    }
+    return false;
   }
 }
