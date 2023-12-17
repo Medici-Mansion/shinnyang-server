cba078805a67ccd8b86220a402fb785c0586758c
Raymond <akdfhr2@gmail.com>
2023-12-17 23:19:45 +0900

    feat: 구글 로그인

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AuthRepository } from './auth.repository';
 import { HttpModule } from '@nestjs/axios';
 @Module({
   imports: [HttpModule],
-  controllers: [AuthController],
   providers: [AuthService, AuthRepository],
 })
 export class AuthModule {}
