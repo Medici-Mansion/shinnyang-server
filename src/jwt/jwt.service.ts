f8cc38003e9ca5c47ff5f895ad6a5b9f86cd6593
Raymond <akdfhr2@gmail.com>
2023-12-17 20:04:48 +0900

    feat: get google user info

--- src/jwt/jwt.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { Payload } from 'src/auth/interface/payload.interface';
-import { User } from 'src/entities/user.entity';
-
-@Injectable()
-export class JwtService {}
