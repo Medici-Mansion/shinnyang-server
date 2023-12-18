d10518ba5ed2266a6f1d95eb107b7ab87a1bc64a
raymondanything <akdfhr2@gmail.com>
2023-12-18 17:44:42 +0900

    feat: oauth 인증 에러처리

--- src/oauth/oauth.service.ts
+++ src/oauth/oauth.service.ts
@@ -40,36 +40,40 @@ export class OauthService {
   }
 
   async userFromGoogle(code: string) {
-    const form = new FormData();
-    form.append('client_id', process.env.GOOGLE_AUTH_CLIENT_ID);
-    form.append('client_secret', process.env.GOOGLE_AUTH_CLIENT_SECRET);
-    form.append('code', code);
-    form.append('grant_type', 'authorization_code');
-    form.append('redirect_uri', process.env.GOOGLE_REDIRECT_URL);
-    const response = await axios.post(
-      'https://oauth2.googleapis.com/token',
-      form,
-    );
-    if (!response.data['access_token']) {
-      throw new BadRequestException('Access-Token을 받아오지 못 했습니다.');
-    }
-    // 회원정보 가져오기
-    const userUrl = 'https://www.googleapis.com/oauth2/v2/userinfo';
-    const userResponse = await axios.get(userUrl, {
-      params: {
-        access_token: response.data['access_token'],
-      },
-    });
+    try {
+      const form = new FormData();
+      form.append('client_id', process.env.GOOGLE_AUTH_CLIENT_ID);
+      form.append('client_secret', process.env.GOOGLE_AUTH_CLIENT_SECRET);
+      form.append('code', code);
+      form.append('grant_type', 'authorization_code');
+      form.append('redirect_uri', process.env.GOOGLE_REDIRECT_URL);
+      const response = await axios.post(
+        'https://oauth2.googleapis.com/token',
+        form,
+      );
+      if (!response.data['access_token']) {
+        throw new BadRequestException('Access-Token을 받아오지 못 했습니다.');
+      }
+      // 회원정보 가져오기
+      const userUrl = 'https://www.googleapis.com/oauth2/v2/userinfo';
+      const userResponse = await axios.get(userUrl, {
+        params: {
+          access_token: response.data['access_token'],
+        },
+      });
 
-    const googleUesr = userResponse.data as GoogleUserInfo;
+      const googleUesr = userResponse.data as GoogleUserInfo;
 
-    let user = await this.userService.findByUserEmail(googleUesr.email);
-    if (!user) {
-      user = await this.userService.create({ email: googleUesr.email });
+      let user = await this.userService.findByUserEmail(googleUesr.email);
+      if (!user) {
+        user = await this.userService.create({ email: googleUesr.email });
+      }
+      const token = this.authService.sign(user.id);
+      user.refresh = token.refresh;
+      await this.dataSource.getRepository(User).save(user);
+      return new GoogleAuthResponse(token, user);
+    } catch (err) {
+      throw new BadRequestException('invalid request');
     }
-    const token = this.authService.sign(user.id);
-    user.refresh = token.refresh;
-    await this.dataSource.getRepository(User).save(user);
-    return new GoogleAuthResponse(token, user);
   }
 }
