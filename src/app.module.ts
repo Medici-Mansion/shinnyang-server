b49f6362f8a778ddff8e0e8a6c62f66d08c3a730
raymondanything <akdfhr2@gmail.com>
2023-12-18 14:47:12 +0900

    feat: 공통코드 제작용 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
@@ -8,23 +7,25 @@ import { LettersModule } from './letters/letters.module';
 import { UserModule } from './users/user.module';
 import { AuthModule } from './auth/auth.module';
 import { OauthModule } from './oauth/oauth.module';
-import * as joi from 'joi';
+import { CommonModule } from './common/common.module';
+import Joi from '@hapi/joi';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
-      validationSchema: joi.object({
-        GOOGLE_API_KEY: joi.string().required(),
-        GOOGLE_AUTH_CLIENT_ID: joi.string().required(),
-        GOOGLE_AUTH_CLIENT_SECRET: joi.string().required(),
-        GOOGLE_REDIRECT_URL: joi.string().required(),
-        DB_HOST: joi.string().required(),
-        DB_PORT: joi.string().required(),
-        DB_USER: joi.string().required(),
-        DB_PWD: joi.string().required(),
-        DB_NAME: joi.string().required(),
-        PORT: joi.string().required(),
+      validationSchema: Joi.object({
+        GOOGLE_API_KEY: Joi.string().required(),
+        GOOGLE_AUTH_CLIENT_ID: Joi.string().required(),
+        GOOGLE_AUTH_CLIENT_SECRET: Joi.string().required(),
+        GOOGLE_REDIRECT_URL: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_PWD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        PORT: Joi.string().required(),
       }),
     }),
     // 아직 어떤 db를 쓸지 정하지 않았음
@@ -33,6 +34,7 @@ import * as joi from 'joi';
     UserModule,
     AuthModule,
     OauthModule,
+    CommonModule,
   ],
   controllers: [AppController],
   providers: [],
