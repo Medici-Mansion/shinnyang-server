55bf4a9c17b6fc79893b76ab225e2d016cb82cb7
raymondanything <akdfhr2@gmail.com>
2023-12-27 16:12:08 +0900

    feat: 닉네임 변경 시 6글자 제한

--- src/users/dtos/set-nickname.dto.ts
+++ src/users/dtos/set-nickname.dto.ts
@@ -1,8 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsString, MaxLength } from 'class-validator';
 
 export class ChangeNicknameDTO {
   @ApiProperty({ description: '번경할 닉네임', default: '츄츄' })
   @IsString()
+  @MaxLength(6)
   nickname: string;
 }
